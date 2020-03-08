d14c20e1a475e9904adac80842d50501ec2589ca
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-08 20:34:21 +0300

    добавлен параметризованный тест

--- src/test/java/com/github/TheDebofNight19/TestNGTest.java
+++ src/test/java/com/github/TheDebofNight19/TestNGTest.java
@@ -1,6 +1,7 @@
 package com.github.TheDebofNight19;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestNGTest {
@@ -9,5 +10,26 @@ public class TestNGTest {
     @Test
     public void test1(){
         LOG.info("This is a testNG test!");
+    }
+
+    /**
+     * Параметризованный тест
+     * @return
+     */
+
+    @DataProvider(name = "Data Provider")
+    public Object[] dataProvider(){
+
+        return new Object[][]{
+                {"Test No", 1},
+                {"Test No", 2}
+        };
+    }
+
+    @Test(dataProvider = "Data Provider")
+    public void test2(String test, int num){
+        LOG.info("This is another testNG test!");
+        String print = String.format("TEST: %s | NOM: %d", test, num);
+        System.out.println(print);
     }
 }
